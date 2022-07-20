cde9a20481060bf2d7eed89ec689ffb201034440
Donovan Duperrois <slashhable@gmail.com>
2022-07-20 09:12:47 +0200

    corrections finales

--- routes/task-router.js
+++ routes/task-router.js
@@ -5,17 +5,17 @@ const taskRouter = express.Router();
 
 taskRouter.route('/')
     .get((req,res)=> {res.sendStatus(501);})
-    .put((req,res)=> {res.sendStatus(501);})
+    .post((req,res)=> {res.sendStatus(501);})
 
 taskRouter.route('/:id')
     .get((req,res)=> {res.sendStatus(501);})
     .put((req,res)=> {res.sendStatus(501);})
     .delete((req,res)=> {res.sendStatus(501);})
 
-taskRouter.route('/categoryname')
+taskRouter.route('/:categoryname')
     .get((req,res)=> {res.sendStatus(501);})
 
-taskRouter.route('/username')
+taskRouter.route('/:username')
     .get((req,res)=> {res.sendStatus(501);})
     
 
